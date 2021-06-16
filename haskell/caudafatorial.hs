376dcac0bd6527b22f47342ffb2216487b11efdd
Fernanda Lopes <fpl.snf19@uea.edu.br>
2021-06-16 15:51:52 -0400

    adiciona descrição

--- haskell/caudafatorial.hs
+++ haskell/caudafatorial.hs
@@ -1,5 +1,5 @@
-fat n = fat’ n 1
+fat n = fat’ n 1 --(1) é usado para inicializar a recursão e garantir a chamada recursiva
     where
-        fat’ n x
+        fat’ n x --é passado o número para cálculo do fatorial e o acumulador para a recursão
         | n == 0 = x
-        | n > 0 = fat’ (n-1) (n*x)
+        | n > 0 = fat’ (n-1) (n*x) --chamada recursiva do calculo fatorial
--- haskell/imparpar.hs
+++ haskell/imparpar.hs
@@ -1,1 +1,3 @@
+{-a esquerda ficam a lista dos números divididos por 2 com resto diferente de 0 (impares);
+a direita se encontra a lista dos números divididos 2 com resto igual a 0 (pares).-}
 imparpar xs = [[x | x <-xs, mod x 2 /= 0], [x | x <-xs, mod x 2 == 0]]
