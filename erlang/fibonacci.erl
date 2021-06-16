04a62c4d58367ee8a2af93d952bd0192b23d6932
Dayvson Silva <userddssilva@gmail.com>
2021-06-16 00:24:18 -0400

    e-2: fiboncci

--- erlang/fibonacci.erl
+++ erlang/fibonacci.erl
@@ -1,6 +1,9 @@
 -module(fibonacci).
 -export([fib/1]).
 
+%* if n = 0, return 0
+%* if n = 1, return 1
+%* else return fib(n-1) + fib(n-2)
 fib(0) -> 0;
 fib(1) -> 1;
 fib(N) -> fib(N-1) + fib(N-2).
