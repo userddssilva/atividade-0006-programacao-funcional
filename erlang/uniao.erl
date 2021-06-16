a6f576d5e396593c826b890b6a2f8a0869a2ff0c
Dayvson Silva <userddssilva@gmail.com>
2021-06-16 00:30:25 -0400

    e-4: uniao

--- erlang/uniao.erl
+++ erlang/uniao.erl
@@ -2,5 +2,7 @@
 -import(lists, [umerge/1]).
 -export([uniao/2]).
 
+%* takes 2 lists as arguments
+%* returns the union of the two lists without repetitions
 uniao(ListA, ListB) ->
 	umerge([ListA, ListB]).
