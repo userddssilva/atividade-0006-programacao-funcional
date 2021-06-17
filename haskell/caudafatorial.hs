f80c9f019c055aafc5cefda4569d3328c5394b86
Fernanda Lopes <fpl.snf19@uea.edu.br>
2021-06-16 20:13:01 -0400

    ajuste do fatorial em cauda

--- haskell/caudafatorial.hs
+++ haskell/caudafatorial.hs
@@ -1,5 +1,5 @@
 fat n = fataux n 1 --(1) é usado para inicializar a recursão e garantir a chamada recursiva
     where
-        fataux n x --é passado o número para cálculo do fatorial e o acumulador para a recursão
+        fataux n x --É passado o número para cálculo do fatorial e o acumulador para a recursão
             | n == 0 = x
-            | n > 0 = fataux (n-1) (n*x) --chamada recursiva do calculo fatorial
+            | n > 0 = fataux (n-1) (n*x) --Chamada recursiva do calculo fatorial
